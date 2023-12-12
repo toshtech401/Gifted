d74cfe6744165b632c0eef974708ef78bdc6f3a8
Ola Tech <mustaphas902@.com>
2023-12-12 10:53:51 +0100

    adding a dependency

--- Controller/userDashboard.js
+++ Controller/userDashboard.js
@@ -3,8 +3,19 @@
 
 const userDashboard = async(req, res) => {
     const user = req.user
+    const totalPoints = await Points.find()
+    const totalEarnings = await Earnings.find()
+    const referrals = await Referrals.find()
+
+
+    const userData = {
+        totalEarnings,
+        totalPoints,
+        referrals
+    }
 
     if(!user) throw new Error("User Not Found!!")
+    return res.json({data: userData})
 }
 
 
